b282ec7399a4fbedeebfd356e5eeb67535746650
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-11-17 19:17:03 -0300

    feat: create account model interface

--- backend/src/models/account/create/CreateAccountModel.ts
+++ backend/src/models/account/create/CreateAccountModel.ts
@@ -1,13 +1,15 @@
+import { AccountType } from '../../../@types/account'
 import Account from '../../../sequelize/models/Account'
+import ICreateAccountModel from './interface/ICreateAccountModel'
 
-export default class CreateAccountModel {
+export default class CreateAccountModel implements ICreateAccountModel<AccountType> {
   private _accountRepository: typeof Account
 
   constructor (accountRepository: typeof Account) {
     this._accountRepository = accountRepository
   }
 
-  public execute = async () => {
+  public execute = async (): Promise<AccountType> => {
     const newAccount = await this._accountRepository.create()
 
     return newAccount
