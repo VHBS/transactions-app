c79dcd2cccba7b52568d821347bbf7b7464c056c
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-11-17 19:17:20 -0300

    feat: create user model interface

--- backend/src/models/user/create/CreateUserModel.ts
+++ backend/src/models/user/create/CreateUserModel.ts
@@ -1,14 +1,15 @@
 import { UserType } from '../../../@types/user'
 import User from '../../../sequelize/models/User'
+import ICreateUserModel from './interface/ICreateUserModel'
 
-export default class CreateUserModel {
+export default class CreateUserModel implements ICreateUserModel<UserType> {
   private _userRepository: typeof User
 
   constructor (userRepository: typeof User) {
     this._userRepository = userRepository
   }
 
-  public execute = async (user: UserType) => {
+  public execute = async (user: UserType): Promise<UserType> => {
     const newUser = await this._userRepository.create(user)
 
     return newUser.getData
