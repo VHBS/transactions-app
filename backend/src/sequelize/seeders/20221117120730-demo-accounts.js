188ee2b46d51a9c6b9139f507deb97253a1d4267
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-11-17 15:30:47 -0300

    fix: removing account and user timestamps seeders

--- backend/src/sequelize/seeders/20221117120730-demo-accounts.js
+++ backend/src/sequelize/seeders/20221117120730-demo-accounts.js
@@ -6,21 +6,15 @@ module.exports = {
     await queryInterface.bulkInsert('Accounts', [
       {
         id: 'd8723224-e445-4ccc-aeb3-a4ad55462c84',
-        balance: 10000,
-        created_at: new Date(),
-        updated_at: new Date()
+        balance: 10000
       },
       {
         id: '23a5999d-dbfc-4960-ac4d-9ec2b91a302c',
-        balance: 20000,
-        created_at: new Date(),
-        updated_at: new Date()
+        balance: 20000
       },
       {
         id: '88ac8439-1c23-4902-b11a-534cf0411d91',
-        balance: 30000,
-        created_at: new Date(),
-        updated_at: new Date()
+        balance: 30000
       }
     ])
   },
--- backend/src/sequelize/seeders/20221117134458-demo-users.js
+++ backend/src/sequelize/seeders/20221117134458-demo-users.js
@@ -8,25 +8,19 @@ module.exports = {
         id: 'ad2f6f78-6172-4c6d-969c-3adc3d9032d5',
         user_name: 'Victor',
         account_id: 'd8723224-e445-4ccc-aeb3-a4ad55462c84',
-        password: '$2a$10$k5Vw.FdvWOsK1ELnRlTjOu75IGeLdfcK04KcGOx.oO/8A77EBrb5K', // 123456
-        created_at: new Date(),
-        updated_at: new Date()
+        password: '$2a$10$k5Vw.FdvWOsK1ELnRlTjOu75IGeLdfcK04KcGOx.oO/8A77EBrb5K' // 123456
       },
       {
         id: 'e3ffbca9-13e3-4e37-8d85-5c3cd581215a',
         user_name: 'Erick',
         account_id: '23a5999d-dbfc-4960-ac4d-9ec2b91a302c',
-        password: '$2a$10$zifBA5/5u6QTs3laFa30LOhXTJyobb.c9FEgPxXfm9Ps5NTuuFEGW', // 654321
-        created_at: new Date(),
-        updated_at: new Date()
+        password: '$2a$10$zifBA5/5u6QTs3laFa30LOhXTJyobb.c9FEgPxXfm9Ps5NTuuFEGW' // 654321
       },
       {
         id: 'e5a99a6c-5768-4f7a-babe-6245e8abef6d',
         user_name: 'Thauler',
         account_id: '88ac8439-1c23-4902-b11a-534cf0411d91',
-        password: '$2a$10$ZAbVHpeB49fv4b8Js5Vbb.3YbuP5HPgybLCM/0HU.YhjItW0GHcFa', // 111111
-        created_at: new Date(),
-        updated_at: new Date()
+        password: '$2a$10$ZAbVHpeB49fv4b8Js5Vbb.3YbuP5HPgybLCM/0HU.YhjItW0GHcFa' // 111111
       }
     ])
   },
