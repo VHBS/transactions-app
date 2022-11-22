f6253633e9f4bfb3f97a0ef8d191c3cf6d12ff9f
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-11-22 20:11:29 -0300

    feat: register new user with private routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import PrivateRouter from './router/PrivateRouter'
 
-function App() {
+function App (): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/" element={<PrivateRouter />}>
+        <Route path="/" element={<Home />} />
+      </Route>
+    </Routes>
+  )
 }
 
-export default App;
+export default App
