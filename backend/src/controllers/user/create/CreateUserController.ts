1c66fa357552f2de8dba17d5a9ba5a0c669bb033
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-11-28 11:50:44 -0300

    fix: create user controller returns Auth User type

--- backend/src/controllers/user/create/CreateUserController.ts
+++ backend/src/controllers/user/create/CreateUserController.ts
@@ -1,22 +1,27 @@
 import { NextFunction, Request, Response } from 'express'
-import { CreateUserServiceType } from '../../../@types/user'
+import { MessageErrorType } from '../../../@types/error'
+import { AuthUserServiceType } from '../../../@types/user'
 import ICreateUserService from '../../../services/user/create/interface/ICreateUserService'
 import ICreateUserController from './interface/ICreateUserController'
 
-export default class CreateUserController implements ICreateUserController<CreateUserServiceType> {
-  private _createUserService: ICreateUserService<CreateUserServiceType>
+export default class CreateUserController implements ICreateUserController<AuthUserServiceType> {
+  private _createUserService: ICreateUserService<AuthUserServiceType>
 
-  constructor (createUserService: ICreateUserService<CreateUserServiceType>) {
+  constructor (createUserService: ICreateUserService<AuthUserServiceType>) {
     this._createUserService = createUserService
   }
 
-  public execute = async (req: Request, res: Response, next: NextFunction) => {
+  public execute = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response<AuthUserServiceType | MessageErrorType> | void> => {
     try {
       const { userName, password } = req.body
 
       const createUser = await this._createUserService.execute({ userName, password })
 
-      if (!createUser) return res.status(409).json({ message: 'User already exists' })
+      if (!createUser) return res.status(409).json({ message: 'user already exists' })
 
       return res.status(201).json(createUser)
     } catch (error) {
